683790b66fbeffa5d5e507931c48dcbe95b41148
37067 <37067@LAPTOP-PHEKU5ND>
2022-09-13 18:23:06 +0300

    Add project files.

---
+++ Lesson05_GitHub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson05_GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pirmasis;
+            double treciasis;
+        }
+    }
+}
