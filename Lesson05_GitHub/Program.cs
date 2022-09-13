c606aabb554861c3bdf9c585f804f0f66dd7895e
Kajus Sciaponis <as_kajus@yahoo.com>
2022-09-13 21:38:18 +0300

    nauja eilute

--- Lesson05_GitHub/Program.cs
+++ Lesson05_GitHub/Program.cs
@@ -7,9 +7,26 @@ namespace Lesson05_GitHub
         static void Main(string[] args)
         {
 
+            double variable3= 333;
+            double a = 10.0;
+            double b = 15.0;
+            double c;
+            double sum = 0;
+
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Console.WriteLine(c);
+            variable3 = c;
+
+            //double sum = variable3 + variable2 + variable;
+            Console.WriteLine($"Suma: {sum}");
+            double average = sum / 3;
+            Console.WriteLine($"Vidurkis: {sum}");
+
             int spindulys = 6371;
             double variable = 4 / 3 * Math.PI * Math.Pow(spindulys, 3);
             Console.WriteLine($"rutulio turis V = {variable}, kai spindulio ilgis {spindulys}km");
+            Console.WriteLine();
+
 
         }
     }
