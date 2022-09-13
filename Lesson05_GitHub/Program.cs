d21b684ae44693b1b30f6f88ba0e1ca34b4086e5
Kajus Sciaponis <as_kajus@yahoo.com>
2022-09-13 18:59:03 +0300

    pridejau kintamaji

--- Lesson05_GitHub/Program.cs
+++ Lesson05_GitHub/Program.cs
@@ -5,9 +5,11 @@ namespace Lesson05_GitHub
     internal class Program
     {
         static void Main(string[] args)
-        {
-            double pirmasis;
-            double treciasis;
+        {   
+
+            double pirmasis = 1;
+            double antrasis = 2;
+            double treciasis = 3;
         }
     }
 }
