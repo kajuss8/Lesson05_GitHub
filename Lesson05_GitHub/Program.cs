7fcd5b69318e92870cda6ea96fbaac68dae91219
Kajus Sciaponis <as_kajus@yahoo.com>
2022-09-13 20:28:53 +0300

    skritulio turis

--- Lesson05_GitHub/Program.cs
+++ Lesson05_GitHub/Program.cs
@@ -5,11 +5,12 @@ namespace Lesson05_GitHub
     internal class Program
     {
         static void Main(string[] args)
-        {   
+        {
 
-            double pirmasis = 1;
-            double antrasis = 2;
-            double treciasis = 3;
+            int spindulys = 6371;
+            double variable = 4 / 3 * Math.PI * Math.Pow(spindulys, 3);
+            Console.WriteLine($"rutulio turis V = {variable}, kai spindulio ilgis {spindulys}km");
+
         }
     }
 }
